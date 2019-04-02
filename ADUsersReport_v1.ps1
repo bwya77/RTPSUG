0f0018b0df2c4db07b5aae9707276278d5521ce9
Bradley Wyatt <bradleywyatt1@gmail.com>
2019-04-01 20:00:54 -0500

    Create ADUsersReport_v1.ps1

---
+++ ADUsersReport_v1.ps1
@@ -0,0 +1,34 @@
+﻿$UsersTable = New-Object 'System.Collections.Generic.List[System.Object]'
+
+
+Get-ADUser -Filter * -Properties * | ForEach-Object {
+	$obj = [PSCustomObject]@{
+		
+		'Name' = $_.Name
+		'UserPrincipalName' = $_.UserPrincipalName
+		'Enabled' = $_.Enabled
+		'Last Logon' = $_.PasswordNeverExpires
+	}
+	
+	$UsersTable.Add($obj)
+}
+
+
+if (($UsersTable).Count -eq 0)
+{
+	
+	$Obj = [PSCustomObject]@{
+		
+		Information = 'Information: No users were found in Active Directory'
+	}
+	$UsersTable.Add($obj)
+}
+
+
+$FinalReport = New-Object 'System.Collections.Generic.List[System.Object]'
+$FinalReport.Add($(Get-HTMLOpenPage -TitleText "AD Users Report"))
+$FinalReport.Add($(Get-HTMLContentOpen -HeaderText "Users"))
+$FinalReport.Add($(Get-HTMLContentTable $UsersTable))
+$FinalReport.Add($(Get-HTMLContentClose))
+$FinalReport.Add($(Get-HTMLClosePage))
+Save-HTMLReport -ReportContent $FinalReport -ShowReport -ReportName "AD Users Report" -ReportPath C:\Automation\Users.HTML
